158bd60ba823edcc57976ff33fd28f2c3c666a8a
Joonho Ko <joonhok@mit.edu>
2018-04-14 03:35:45 -0400

    Add files via upload

--- tiny_spidey.py
+++ tiny_spidey.py
@@ -3,34 +3,31 @@ from urllib.request import urlopen
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup		# I love BeautifulSoup and I only discovered it 20 minutes ago.
 
-# ACTUAL CODE
+def crawl(start, iters): 
+	"""
+	@param start: full URL from where to start random walk
+	@param iters: number of iterations/timesteps in the random walk
+	@returns: list of URLs the walk visited, including start URL
+	"""
+	output = []
+	link = start		# initialize link
+	last_link = start	# useful pointer to fall back to
 
-print("~ Tiny Spidey 1.0 ~")
-link = input("(Full) start URL to crawl: ")			# get start HTML file ('start state')
-iters = int(input("Number of (max) iterations: "))	# number of iterations of random walk
-walks = int(input("Number of walks: "))				# number of random walks (with iters iterations) to make
-
-start_link = link 	# remember for future walks
-
-for w in range(walks):
-	link = start_link
-	last_link = start_link	 
-	print("Commencing traversal number "+str(w)+".\n")
 	for _ in range(iters): 
-		try:													# if opening link is troublesome, revert to last link
-			file = urlopen(link)								# open link, get html file
+		try:						# if opening link is troublesome, revert to last link
+			file = urlopen(link)	# open link, get html file
 		except:
-			link = last_link									# continue from last link
+			link = last_link		# continue from last link
 			continue
 		soup = BeautifulSoup(file, 'html.parser')				# get soup
 		links = [l.get('href') for l in soup.find_all('a')]		# get list of links in page
-		if len(links) == 0:										# if there are no links to go to:
-			link = last_link
+		if len(links) == 0:										# if there are no more links to go to:
+			link = last_link									# try again from previous link
 			continue
 		links = [l for l in links if bool(urlparse(l).netloc)]	# reduce to only absolute links (more interesting)
 		url = random.choice(links)								# pick next URL u.a.r
 		link = urljoin(link, url)								# make accessible to future iterations
+		output.append(link)
 
-		print("Traveling to: "+link)
+	return output
 
-	print("Traversal complete.\n")	# done with traversal
